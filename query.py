376d71d8ae643b16f6cc990b0bec2674a047cfd9
EduardoLPaez <edpaez54g5@gmail.com>
2020-06-13 19:02:03 -0400

    added squareup, gods I wish I found earlier.

--- query.py
+++ query.py
@@ -1,25 +1,23 @@
 import pandas as pd
 import numpy as np
-from secrets import Dev_name, Dev_secret, test_id
+from secrets import test_id, dev_pr_secret, iqn, iqs, dev_name, dev_secret
 import requests
+from square.client import Client
+
+pd.set_option('display.max_columns', None)
+pd.set_option('max_colwidth', None)
 #import kivy
 # square api
 # json seems to be the above return
-
+client = Client(
+    access_token = dev_secret,
+    environment = 'sandbox'
+)
 
 def pull_invoice(id_ = test_id):
-
-    payment_id = id_ # will be testing with live after sandbox
-        #(got personal issuses with the amount of human err it introduces.)
-    # bellow generates the required for responce(halley dam lulla).
-    header = {'Content-Type': 'application/json',
-           'Authorization': 'Bearer {0}'.format(Dev_secret)}
-    # get request ussing the above credentials with (currently) test target id_.
-    result = requests.get('https://connect.squareupsandbox.com/v2/payments/{payment_id}', 
-    headers=header
-    )
+    result = client.payments.get_payment( payment_id = id_ )
     # gotta love pandas.
-    df = pd.DataFrame(result.json())
+    df = pd.DataFrame(result.body)
     return df
     
     
