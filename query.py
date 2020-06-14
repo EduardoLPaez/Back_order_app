916edcb3196da1f583dad249b4538e67f52c3b71
EduardoLPaez <edpaez54g5@gmail.com>
2020-06-14 12:17:28 -0400

    paument methot fell through, not enough deatil.

--- query.py
+++ query.py
@@ -1,24 +1,45 @@
 import pandas as pd
 import numpy as np
-from secrets import test_id, dev_pr_secret, iqn, iqs, dev_name, dev_secret
-import requests
+from secrets import test_id, dev_name, dev_secret
+from secrets import dev_pr_secret, iq_permission
 from square.client import Client
+import datetime as dt
 
 pd.set_option('display.max_columns', None)
 pd.set_option('max_colwidth', None)
 #import kivy
-# square api
-# json seems to be the above return
-client = Client(
-    access_token = dev_secret,
-    environment = 'sandbox'
-)
+# this is screwed, square api only permits spesific search for payment id and only that.
+# 
+def pull_invoice():#reset it to the last 5 sales
+    client = Client(
+        access_token = dev_pr_secret,
+    )
+    result = client.orders.batch_retrieve_orders(
+    location_id = test_id, 
+    body = {
+        client.orders.search_orders(
+            body = {
+                'location_ids' : test_id,
+                "query": {
+                    "filter": {
+                        "date_time_filter": {
+                            "created_at": {
+                                "start_at": (dt.datetime.today() - dt.timedelta(days=1)),
+                                "end_at": dt.datetime.today()
+        }}}}})})
+    df = pd.DataFrame(result.body)
+    return df.head(5)
 
-def pull_invoice(id_ = test_id):
+
+def sandbox_pull_invoice(id_ = test_id):
+    client = Client(
+        access_token = dev_secret,
+        environment = 'sandbox'
+    )
     result = client.payments.get_payment( payment_id = id_ )
     # gotta love pandas.
     df = pd.DataFrame(result.body)
     return df
     
     
-print(pull_invoice())
+print(pull_invoice().columns)
